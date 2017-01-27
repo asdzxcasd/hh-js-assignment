81d5f7979cbbf542db79a9c8f36a1d28d93d549e
Fedor <ermishinf2@gmail.com>
2017-01-28 00:36:04 +0300

    Made fixes as suggested

--- solution.js
+++ solution.js
@@ -1,12 +1,10 @@
-function Graph() {}
-
 // Evaluates vertex with name vertexName
-Graph.prototype.evaluateLazy = function(vertexName) {
-    var vertex = this[vertexName];
+function evaluateLazy(graph, vertexName) {
+    var vertex = graph[vertexName];
     if (vertex === undefined) {
         throw new Error("Dependency missing: " + vertexName);
     }
-    if (!vertex.isEvaluated) {
+    if (vertex.computedValue === undefined) {
         if (vertex.isComputing) {
             throw new Error("Cyclic dependency in graph");
         }
@@ -14,42 +12,20 @@ Graph.prototype.evaluateLazy = function(vertexName) {
         var args = []
         for (var i = 0; i < vertex.dependencies.length; i++) {
             var name = vertex.dependencies[i];
-            args.push(this.evaluateLazy(name));
+            args.push(evaluateLazy(graph, name));
         }
-        vertex.computedValue = vertex.calculationFunction.apply(this, args);
+        vertex.computedValue = vertex.calculationFunction.apply(vertex, args);
         vertex.isComputing = false;
-        vertex.isEvaluated = true;
     }
     return vertex.computedValue;
 }
 
 // Evaluates all vertices of the graph and returns map of obtained values
-Graph.prototype.evaluateAll = function() {
+function evaluateAll(graph) {
     result = {}
-    graph = this;
-    function rec(key) {
-        var vertex = graph[key];
-        if (vertex === undefined) {
-            throw new Error("Dependency missing: " + vertexName);
-        }
-        if (!vertex.isEvaluated) {
-            if (vertex.isComputing) {
-                throw new Error("Cyclic dependency in graph");
-            }
-            vertex.isComputing = true;
-            var args = []
-            for (var i = 0; i < vertex.dependencies.length; i++) {
-                var name = vertex.dependencies[i];
-                args.push(rec(name));
-            }
-            vertex.computedValue = vertex.calculationFunction.apply(this, args);
-            vertex.isComputing = false;
-            vertex.isEvaluated = true;
-            result[key] = vertex.computedValue;
-        }
-        return vertex.computedValue;
-    }
-    Object.keys(graph).forEach(rec);
+    Object.keys(graph).forEach(function(key) {
+        result[key] = evaluateLazy(graph, key);
+    });
     return result;
 }
 
@@ -57,7 +33,6 @@ Graph.prototype.evaluateAll = function() {
 // dependencies is an array of strings
 // calculationFunction is a function with arguments, which correspond to dependencies
 function Vertex(dependencies, calculationFunction) {
-    this.isEvaluated = false;
     this.isComputing = false;
     this.dependencies = dependencies;
     this.calculationFunction = calculationFunction;
@@ -65,7 +40,7 @@ function Vertex(dependencies, calculationFunction) {
 
 // Creates sample graph from article http://plumatic.github.io/prismatics-graph-at-strange-loop/
 function sampleGraph() {
-    var graph = new Graph();
+    var graph = {};
     graph.n = new Vertex(['xs'], function(xs) {return xs.length;});
     graph.m = new Vertex(['xs', 'n'], function(xs, n) {return xs.reduce((a, b) => a + b, 0) / n;});
     graph.m2 = new Vertex(['xs', 'n'], function(xs, n) {return xs.map(x => x * x).reduce((a, b) => a + b, 0) / n;});
@@ -77,13 +52,13 @@ function sampleGraph() {
 // Creates sample graph and evaluates a vertex lazily
 function runLazy() {
     graph = sampleGraph();
-    console.log(graph.evaluateLazy('v'));
+    console.log(evaluateLazy(graph, 'v'));
 }
 
 // Creates sample graph and evauates all vertices
 function run() {
     graph = sampleGraph();
-    console.log(graph.evaluateAll());
+    console.log(evaluateAll(graph));
 }
 
 runLazy();
